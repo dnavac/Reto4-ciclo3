2a45b59a48070271f150614ee396cbbc910f2d03
DiegoN <dnavarro@araujoysegovia.com>
2024-05-16 10:05:55 -0500

    first commit

---
+++ src/main/java/com/example/demo/Reto4Ciclo3Application (2022_04_27 06_57_24 UTC).java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto4Ciclo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto4Ciclo3Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/entidades/registro (2022_04_27 06_57_24 UTC).java
@@ -0,0 +1,55 @@
+package com.example.demo.entidades;
+
+public class registro {
+	private int REGIdentifica;
+	private String REGNombres;
+	private String REGApellidos;
+	private String REGCorreo;
+	private int REGMovil;
+	private String REGDireccion;
+	private int REGMunicipio;
+	public int getREGIdentifica() {
+		return REGIdentifica;
+	}
+	public void setREGIdentifica(int rEGIdentifica) {
+		REGIdentifica = rEGIdentifica;
+	}
+	public String getREGNombres() {
+		return REGNombres;
+	}
+	public void setREGNombres(String rEGNombres) {
+		REGNombres = rEGNombres;
+	}
+	public String getREGApellidos() {
+		return REGApellidos;
+	}
+	public void setREGApellidos(String rEGApellidos) {
+		REGApellidos = rEGApellidos;
+	}
+	public String getREGCorreo() {
+		return REGCorreo;
+	}
+	public void setREGCorreo(String rEGCorreo) {
+		REGCorreo = rEGCorreo;
+	}
+	public int getREGMovil() {
+		return REGMovil;
+	}
+	public void setREGMovil(int rEGMovil) {
+		REGMovil = rEGMovil;
+	}
+	public String getREGDireccion() {
+		return REGDireccion;
+	}
+	public void setREGDireccion(String rEGDireccion) {
+		REGDireccion = rEGDireccion;
+	}
+	public int getREGMunicipio() {
+		return REGMunicipio;
+	}
+	public void setREGMunicipio(int rEGMunicipio) {
+		REGMunicipio = rEGMunicipio;
+	}
+
+}
+
